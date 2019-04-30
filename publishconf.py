97d65feb2e71a61388ffd1fdd17bf246ca5b7f1b
Thomi Richards <thomi.richards@zapier.com>
2019-05-01 11:40:18 +1200

    Start serving production over HTTPS.

--- publishconf.py
+++ publishconf.py
@@ -7,15 +7,16 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 
 from common_settings import *
 
-SITEURL = 'http://connascence.io'
+SITEURL = "https://connascence.io"
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
